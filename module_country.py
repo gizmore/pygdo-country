372d95b8c50a572d58f7f7c0c71e90d2b2761a79
gizmore <gizmore@wechall.net>
2025-12-07 11:35:06 +0100

    'profile'

--- module_country.py
+++ module_country.py
@@ -1,6 +1,11 @@
 from gdo.base.GDO_Module import GDO_Module
+from gdo.base.GDT import GDT
+from gdo.core.GDT_Bool import GDT_Bool
 from gdo.country.GDO_Country import GDO_Country
 import csv
+
+from gdo.country.GDT_Country import GDT_Country
+
 
 class module_country(GDO_Module):
 
@@ -22,3 +27,17 @@ class module_country(GDO_Module):
                 bulk_data = [row['name'], row['alpha-2']]
                 bulk.append(bulk_data)
         GDO_Country.table().bulk_insert(headers, bulk)
+
+    def gdo_module_config(self) -> list[GDT]:
+        return [
+            GDT_Bool('country_signup_detection').initial('1'),
+        ]
+
+    def cfg_signup_detection(self) -> bool:
+        return self.get_config_value('country_signup_detection')
+
+    def gdo_user_settings(self) -> list[GDT]:
+        return [
+            GDT_Country('country_ethnics'),
+            GDT_Country('country_living'),
+        ]
